9a5e75f3cfdfb18f0a8b1e03f6c519f1051c875b
aziz-codes <azizisoffline@gmail.com>
2023-08-04 20:13:16 +0500

    replace all and replace method

--- string.js
+++ string.js
@@ -2,4 +2,9 @@
 const email = "info@currenttick.com";
 // let domain = email.substring(email.indexOf("@") - 1);
 
-console.log("info@currenttick.comazizbjhbjb".length);
+// console.log(email.padEnd(30, "azizbjhbjbjbjjbjbjbjb"));
+// console.log("info@currenttick.comazizbjhbjb".length);
+
+let user = "aziz";
+let newUser = user.replaceAll("z", "b");
+console.log(newUser);
