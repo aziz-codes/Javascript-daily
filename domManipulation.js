dd17c6ee77b3c34abcbcb12daa930d688b3648fd
aziz-codes <azizisoffline@gmail.com>
2023-08-05 23:14:38 +0500

    settting outerhtml of button

--- domManipulation.js
+++ domManipulation.js
@@ -2,5 +2,6 @@ const passwordEl = document.getElementById("password");
 const btnEl = document.getElementById("btn");
 btnEl.addEventListener("click", () => {
   passwordEl.type = passwordEl.type === "password" ? "text" : "password";
+  btnEl.outerHTML = "hello";
 });
 // test commit
