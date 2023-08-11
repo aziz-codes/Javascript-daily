e34af0aa989511f9407cb959e72aa8a9e1040773
aziz-codes <azizisoffline@gmail.com>
2023-08-11 22:12:05 +0500

    input value

--- validations.js
+++ validations.js
@@ -1,1 +1,8 @@
 // file for validation
+const textEl = document.getElementById("textEl");
+
+let name = textEl.value;
+const btnSubmit = document.getElementById("btnSubmit");
+btnSubmit.addEventListener("click", () => {
+  console.log(textEl.value);
+});
