790ed1a6b950800c779745e7b8c19ff91e858ae8
aziz-codes <azizisoffline@gmail.com>
2023-08-14 13:15:59 +0500

    using regex for valiation

--- validations.js
+++ validations.js
@@ -1,9 +1,23 @@
 // file for validation
 const textEl = document.getElementById("textEl");
-
-let name = textEl.value;
+document.body.style.padding = "10px";
+let name = "";
+textEl.addEventListener("change", (e) => {
+  name = textEl.value;
+});
 const btnSubmit = document.getElementById("btnSubmit");
 btnSubmit.addEventListener("click", () => {
-  console.log(textEl.value);
+  validateTetx(name);
 });
 // test commit
+
+// Asif Chapali
+//  03434337418
+
+const validateTetx = (text) => {
+  let specs = "0,1,2,3,4,5,6,7,8,9,@,!,/,-,+,_,#,$,%,^,^,&,*,(,)";
+  let newText = specs.replaceAll(",", "").split("");
+  // console.log(name.split(",").some((i) => i === i.toUpperCase()));
+  let upperCaseRegex = "(/[A-Z]/g)";
+  console.log(text.match(upperCaseRegex));
+};
