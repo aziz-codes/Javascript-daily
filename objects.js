11ddf6554ffc858555bae9663237eb20543bb181
aziz-codes <azizisoffline@gmail.com>
2023-09-22 23:28:38 +0500

    using in op in array and obj

--- objects.js
+++ objects.js
@@ -9,15 +9,23 @@ const uniqueKeys = (arr) => {
     }
   }
 
-  let maxObj = {};
-  let max = 0;
-  for (let i in obj) {
-    if (obj[i] > max) {
-      max = obj[i];
-      maxObj[i] = max;
-    }
-  }
-  return maxObj;
+  // let maxObj = {};
+  // let max = 0;
+  // for (let i in obj) {
+  //   if (obj[i] > max) {
+  //     max = obj[i];
+  //     maxObj[i] = max;
+  //   }
+  // }
+  // return maxObj;
+  return obj;
 };
 const array = ["apple", "pear", "apple", "mango", "apple", "pear"];
 console.log(uniqueKeys(array));
+
+let obj = { apple: 2 };
+if (array[0] in obj) {
+  console.log("found");
+} else {
+  console.log("item not found");
+}
