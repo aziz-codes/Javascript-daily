467bf6ddf4fb323f8fe742c9f0086fb87ddf9e62
aziz-codes <azizisoffline@gmail.com>
2023-09-27 18:04:05 +0500

    month

--- jsdate.js
+++ jsdate.js
@@ -1,2 +1,4 @@
-const year = new Date().getFullYear();
-console.log(year);
+// const year = new Date().getFullYear();
+// console.log(year);
+const month = new Date().getMonth() + 1;
+console.log(month);
