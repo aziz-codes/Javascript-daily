1c4069b5a20b9f201a07d1bedc707d05dc910c57
aziz-codes <azizisoffline@gmail.com>
2023-09-27 18:04:55 +0500

    days

--- jsdate.js
+++ jsdate.js
@@ -1,4 +1,7 @@
 // const year = new Date().getFullYear();
 // console.log(year);
-const month = new Date().getMonth() + 1;
-console.log(month);
+// const month = new Date().getMonth() + 1;
+// console.log(month);
+
+const days = new Date().getDay();
+console.log(days);
