bb0cd7ea5814dd2e31c797fe9dc528e2bb8444d0
aziz-codes <azizisoffline@gmail.com>
2023-09-27 18:09:51 +0500

    max num using math method

--- array.js
+++ array.js
@@ -25,7 +25,7 @@ const products = [
 let sum = products.reduce((acc, el) => {
   return (acc += el.price);
 }, 0);
-console.log(sum);
+// console.log(sum);
 
 // returning max num using reduce
 const arr = [2, 3, 4, , 10, 6];
@@ -33,4 +33,8 @@ const arr = [2, 3, 4, , 10, 6];
 let max = arr.reduce((acc, el) => {
   return el > acc ? el : acc;
 });
-console.log(max);
+// console.log(max);
+
+let nums = [4, 2, 5, 7, 9, 1];
+const maxNum = Math.max.apply(null, nums);
+console.log(maxNum);
