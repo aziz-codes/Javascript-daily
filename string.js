3b67500cc97e25785a9d2f3cc5ab5942a290da36
aziz-codes <azizisoffline@gmail.com>
2024-01-01 19:25:50 +0500

    strings js

--- string.js
+++ string.js
@@ -1,5 +1,5 @@
 // all string methods goes here.
-const email = "info@currenttick.com";
+
 // let domain = email.substring(email.indexOf("@") - 1);
 
 // console.log(email.padEnd(30, "azizbjhbjbjbjjbjbjbjb"));
